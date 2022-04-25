2d6be06d111d8e7cb3d3403266a7e844ff59d4fe
franclobo <fjbl2788@gmail.com>
2022-04-25 14:56:36 -0500

    add const addButton and newBook at scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,42 @@ import dataBooks from './databooks.js';
 
 //Agregar funcion add y remove
 
+const addButton = document.querySelector('add');
+const newBook = document.querySelector('.form-control')
+
+class Node {
+  constructor(value, next_node = null) {
+    this.value = value;
+    this.next_node = next_node;
+  }
+}
+
+class LinkedList {
+  // setup value and next_node
+  constructor() {
+    this._head = null;
+    this._tail = null;
+    this._length = 0;
+  }
+
+  add(newBook) {
+    // your code here
+    let node = new Node(newBook);
+    if (!this._head && !this._tail) {
+      this._head = node;
+      this._tail = this._head;
+    } else {
+      this._tail.next = node;
+      this._tail = this._tail.next;
+    }
+    this._length++;
+  }
+};
+
+addButton.addEventListener('click', (event) => {
+  LinkedList(event);
+});
+
 const bookList = document.getElementById('list');
 const printList = () => {
   let str = '';
