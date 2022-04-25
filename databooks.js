6051c07c05d5bed1949338b9ce6ece5ab8467b43
AntonioHincapie <marcohincapiem@gmail.com>
2022-04-25 13:25:04 -0500

    Added dynamic HTML
    -Added script to print list section.
    -Added content to databooks

---
+++ databooks.js
@@ -0,0 +1,9 @@
+const dataBooks = [{
+  bookTitle: ['Lorem ipsum'],
+  bookAuthor: ['Testeroo Testyy'],
+},
+{
+  bookTitle: ['Second book'],
+  bookAuthor: ['Testeroo Testyy'],
+}];
+export default dataBooks;
---
+++ scripts.js
@@ -0,0 +1,16 @@
+import dataBooks from './databooks.js';
+
+//Agregar funcion add y remove
+
+const bookList = document.getElementById('list');
+const printList = () => {
+  let str = '';
+  for (let i = 0; i < dataBooks.length; i += 1) {
+    str += `<p>${dataBooks[i].bookTitle}</p>
+    <p>${dataBooks[i].bookAuthor}</p>
+    <button type="button">Remove</button>`
+  }
+  return str;
+}
+
+bookList.innerHTML = `${printList()}`
