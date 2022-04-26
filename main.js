16cdee2e2977c7a8efc3620c94061d4c53ca55b0
franclobo <fjbl2788@gmail.com>
2022-04-26 11:27:29 -0500

    Add localStorage and function to delete.

--- main.js
+++ main.js
@@ -10,3 +10,34 @@ class Libro {
     this.author = author;
   }
 };
+
+const printList = () => {
+  books.innerHTML = null;
+  if (localStorage.getItem('book')) {
+    dataBooks = JSON.parse(localStorage.getItem('book'));
+  }
+  for (let i = 0; i < dataBooks.length; i += 1) {
+    const bookStorage = document.createElement('div');
+    const bookName = document.createElement('p');
+    const authorName = document.createElement('p');
+    const deleteBtn = document.createElement('button');
+    bookName.textContent = dataBooks[i].title;
+    authorName.textContent = dataBooks[i].author;
+    deleteBtn.textContent = 'Delete';
+    books.appendChild(bookStorage);
+    bookStorage.appendChild(bookName);
+    bookStorage.appendChild(authorName);
+    bookStorage.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', () => {
+      deleteBook(i, bookStorage);
+    })
+  }
+};
+
+printList();
+
+function deleteBook(index, libro) {
+  libro.remove();
+  dataBooks.splice(index, 1);
+  localStorage.setItem('book', JSON.stringify(dataBooks));
+}
