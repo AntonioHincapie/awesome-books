d8f6ba4431213e2d320d676bcfe7c436a48d7e9c
AntonioHincapie <marcohincapiem@gmail.com>
2022-04-26 20:23:35 -0500

    Fixed linters issues

--- main.js
+++ main.js
@@ -9,6 +9,24 @@ if (localStorage.getItem('book')) {
   dataBooks = JSON.parse(localStorage.getItem('book'));
 }
 
+
+class Libro {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  deleteBook(index) {
+    dataBooks.splice(index, 1);
+    localStorage.setItem('book', JSON.stringify(dataBooks));
+  }
+
+  addBook(libro) {
+    dataBooks.push(libro);
+    localStorage.setItem('book', JSON.stringify(dataBooks));
+  }
+}
+
 const printList = () => {
   const libro = new Libro()
   books.innerHTML = null;
@@ -31,30 +49,9 @@ const printList = () => {
   }
 };
 
-class Libro {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-
-  deleteBook (index) {
-    dataBooks.splice(index, 1);
-    localStorage.setItem('book', JSON.stringify(dataBooks));
-  }
-
-  addBook (libro) {
-    dataBooks.push(libro);
-    localStorage.setItem('book', JSON.stringify(dataBooks));
-    printList();
-  }
-}
-
-
 printList();
 
-addBtn.addEventListener ('click', () => {
+addBtn.addEventListener('click', () => {
   const libro = new Libro(titulo.value, autor.value);
   libro.addBook(libro);
-  titulo = '';
-  autor = '';
 });
