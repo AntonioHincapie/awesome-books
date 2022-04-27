f81640b98068fa13b48d53a488b713a9031da0ed
AntonioHincapie <marcohincapiem@gmail.com>
2022-04-27 17:03:09 -0500

    Fixed linters issues

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const booksBtn = document.getElementById('booksBtn');
 const addBookbtn = document.getElementById('addbookBtn');
-const contactBtn = document.getElementById ('contactBtn');
+const contactBtn = document.getElementById('contactBtn');
 const containerBooks = document.getElementById('container');
 const books = document.getElementById('books');
 const addNew = document.getElementById('addbook');
@@ -11,35 +11,35 @@ const addBtn = document.getElementById('btn');
 let dataBooks = JSON.parse(localStorage.getItem('book')) || [];
 
 // This code below was recovered from https://programacion.net/
-function startTime() {
-    var today = new Date();
-    var hr = today.getHours();
-    var min = today.getMinutes();
-    var sec = today.getSeconds();
-    ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
-    hr = (hr == 0) ? 12 : hr;
-    hr = (hr > 12) ? hr - 12 : hr;
-    hr = checkTime(hr);
-    min = checkTime(min);
-    sec = checkTime(sec);
-    document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap; 
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    var curWeekDay = days[today.getDay()];
-    var curDay = today.getDate();
-    var curMonth = months[today.getMonth()];
-    var curYear = today.getFullYear();
-    var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
-    document.getElementById("date").innerHTML = date;
-    
-    var time = setTimeout(function(){ startTime() }, 500);
+function checkTime(i) {
+  if (i < 10) {
+      i = '0' + i;
+  }
+  return i;
 }
 
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
+function startTime() {
+  let today = new Date();
+  let hr = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+  let ap = (hr < 12) ? '<span>AM</span>' : '<span>PM</span>';
+  hr = (hr === 0) ? 12 : hr;
+  hr = (hr > 12) ? hr - 12 : hr;
+  hr = checkTime(hr);
+  min = checkTime(min);
+  sec = checkTime(sec);
+  document.getElementById('clock').innerHTML = hr + ':' + min + ':' + sec + '' + ap; 
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  let curWeekDay = days[today.getDay()];
+  let curDay = today.getDate();
+  let curMonth = months[today.getMonth()];
+  let curYear = today.getFullYear();
+  let date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+  document.getElementById("date").innerHTML = date;
+    
+  let time = setTimeout(function(){ startTime() }, 500);
 }
 // This code above was recovered from https://programacion.net/
 
